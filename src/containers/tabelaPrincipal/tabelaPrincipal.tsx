1f6fde9c84b8196c943ab11ab9278fd73ebdd4b0
marlonseben <marlonseben1@gmail.com>
2025-11-19 23:33:18 -0300

    feat(tabela-principal): cria popover de acoes

--- src/containers/tabelaPrincipal/tabelaPrincipal.tsx
+++ src/containers/tabelaPrincipal/tabelaPrincipal.tsx
@@ -8,6 +8,9 @@ import {
   TableRow,
   TableCell,
   TableBody,
+  Popover,
+  Button,
+  Divider,
 } from "@mui/material";
 import { MdOutlineDehaze } from "react-icons/md";
 import { BotaoLancamento } from "../../components/buttons/botaoLancamento/botaoLancamento";
@@ -16,19 +19,33 @@ import { DialogLancamento } from "../../components/dialogs/dialogLancamento/dial
 import { dadosTeste } from "./tabelaPrincipalMockData";
 
 export const TabelaPrincipal = () => {
-  const [open, setOpen] = useState(false);
+  const [openDialogLancamento, setOpenDialogLancamento] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
   const [tipoSelecionado, setTipoSelecionado] = useState<"receita" | "despesa">(
     "despesa"
   );
 
   const handleAbrirDialogLancamento = (tipo: "receita" | "despesa") => {
     setTipoSelecionado(tipo);
-    setOpen(true);
+    setOpenDialogLancamento(true);
   };
 
   const handleFecharDialogLancamento = () => {
-    setOpen(false);
+    setOpenDialogLancamento(false);
   };
+
+  const handleAbrirPopoverAcoes = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleFecharPopoverAcoes = () => {
+    setAnchorEl(null);
+  };
+
+  const open = !!anchorEl;
+  const id = open ? "popover-acoes" : undefined;
 
   return (
     <>
@@ -105,9 +122,36 @@ export const TabelaPrincipal = () => {
                   <Typography>{row.metodo}</Typography>
                 </TableCell>
                 <TableCell align="left">
-                  <IconButton>
+                  <IconButton onClick={handleAbrirPopoverAcoes}>
                     <MdOutlineDehaze size={22} color="black" />
                   </IconButton>
+                  <Popover
+                    id={id}
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={handleFecharPopoverAcoes}
+                    anchorOrigin={{
+                      vertical: "bottom",
+                      horizontal: "left",
+                    }}
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "center",
+                    }}
+                    slotProps={{
+                      paper: {
+                        elevation: 1,
+                      },
+                    }}
+                  >
+                    <Button>
+                      <Typography color="black">Visualizar</Typography>
+                    </Button>
+                    <Divider />
+                    <Button>
+                      <Typography color="black">Baixar</Typography>
+                    </Button>
+                  </Popover>
                 </TableCell>
               </TableRow>
             ))}
@@ -115,7 +159,7 @@ export const TabelaPrincipal = () => {
         </Table>
       </TableContainer>
       <DialogLancamento
-        open={open}
+        open={openDialogLancamento}
         onClose={handleFecharDialogLancamento}
         tipo={tipoSelecionado}
       />
