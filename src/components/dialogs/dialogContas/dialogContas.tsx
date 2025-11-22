7c237958aa0c25f479a16077b368e3d4a99d53c7
marlonseben <marlonseben1@gmail.com>
2025-11-22 17:24:45 -0300

    feat(dialog-lancamento): criado botao de 'x' para fechar a dialog
    aplicado apenas na dialog de lancamento no momento, pois a de contas ainda não possui conteúdo

--- src/components/dialogs/dialogContas/dialogContas.tsx
+++ src/components/dialogs/dialogContas/dialogContas.tsx
@@ -5,6 +5,7 @@ const DialogContas = (props: DialogContasProps) => {
   return (
     <Dialog open={props.open}>
       <DialogTitle>Cadastro de contas</DialogTitle>
+
       <DialogContent>
         <Button onClick={props.onClose}>Fechar</Button>
       </DialogContent>
